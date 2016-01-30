37a697a8a3b91ae9d6f9fb17d69e40585b22203c
BallisticFrogs <ballisticfrogs@gmail.com>
2016-01-30 04:04:39 +0100

    adding traps

---
+++ Assets/Scripts/TrapRotation.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapRotation : MonoBehaviour {
+
+    public float speedRotation;
+
+    // Use this for initialization
+    void Start () {
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.Rotate(Vector3.up * speedRotation * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/TrapTranslation.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TrapTranslation : MonoBehaviour {
+
+    public float speed;
+    public Transform start;
+    public Transform finish;
+    public GameObject pole;
+
+    private bool returnToStart = false;
+    private Transform activeTarget;
+
+
+	// Use this for initialization
+	void Start () {
+        activeTarget = finish;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 gap = activeTarget.position - pole.transform.position;
+        Vector3 movement = gap.normalized * speed * Time.deltaTime;
+        if (movement.magnitude >= gap.magnitude)
+        {
+            movement = gap;
+        }
+
+        pole.transform.position += movement;
+
+        if ((activeTarget.position - pole.transform.position).magnitude < 0.1f)
+        {
+            if (activeTarget == finish)
+            {
+                activeTarget = start;
+            } else
+            {
+                activeTarget = finish;
+            }
+        }
+	}
+}
