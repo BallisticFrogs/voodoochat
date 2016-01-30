959a9e8a2a7580478cd361f1befb90e3f4d6c85f
BallisticFrogs <ballisticfrogs@gmail.com>
2016-01-30 11:58:12 +0100

    fix enemies Rigidbody

--- Assets/Scripts/SampleAgentScript.cs
+++ Assets/Scripts/SampleAgentScript.cs
@@ -5,14 +5,17 @@ public class SampleAgentScript : MonoBehaviour {
 
     public Transform target;
     NavMeshAgent agent;
+    private Rigidbody rb;
 
 	// Use this for initialization
 	void Awake () {
         agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+        rb.velocity = Vector3.zero;
         agent.SetDestination(target.position);
 	}
 }
