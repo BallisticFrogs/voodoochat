a5a2a8da9d5d40e47205cc5f107e81fb2ccc7ed0
BallisticFrogs <ballisticfrogs@gmail.com>
2016-01-31 11:18:51 +0100

    Add instructions + fade out

---
+++ Assets/Scripts/FadeAway.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeAway : MonoBehaviour
+{
+    public float waitUntilFade;
+    public float fadeDuration;
+
+    private bool fadeStarted = false;
+    private Text textToFadeOut;
+
+    void Start()
+    {
+        textToFadeOut = GetComponent<Text>();
+    }
+
+	// Update is called once per frame
+    private void Update()
+    {
+        if (fadeStarted)
+        {
+            Color color = textToFadeOut.color;
+            color.a -= 1/(fadeDuration*60);
+            textToFadeOut.color = color;
+            if (textToFadeOut.color.a <= 0)
+            {
+                Destroy(textToFadeOut);
+            }
+
+        } else if (Time.time > waitUntilFade)
+        {
+            fadeStarted = true;
+        }
+        
+    }
+}
+
