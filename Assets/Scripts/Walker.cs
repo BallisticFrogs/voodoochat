74656040ef38c4bc2f95be3dfaa7a6b87b0b9a85
BallisticFrogs <ballisticfrogs@gmail.com>
2016-01-31 15:17:12 +0100

    Walking sound (script and assets only)

---
+++ Assets/Scripts/Walker.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Walker : MonoBehaviour
+{
+    private AudioSource audio;
+
+	// Use this for initialization
+	void Awake ()
+	{
+	    audio = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void PlayWalkingSound ()
+	{
+	    if (!audio.isPlaying)
+	    {
+            audio.Play();
+        }
+	}
+}
